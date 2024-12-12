7441aaebfe4ae7a9e15b7f12c29d190d16839f5b
PaoloSmr <139106409+PaoloSmr@users.noreply.github.com>
2024-12-12 11:53:57 +0100

    novità

--- app/components/Card/CardSubtitle.tsx
+++
@@ -1,9 +0,0 @@
-type CardSubtitleProps = {
-	text: string;
-};
-
-const CardSubtitle = ({ text }: CardSubtitleProps) => {
-	return <h3 className='text-2xl font-light text-neutral-50'>{text}</h3>;
-};
-
-export default CardSubtitle;
--- app/components/Card/CardTitle.tsx
+++
@@ -1,9 +0,0 @@
-type CardTitleProps = {
-	text: string;
-};
-
-const CardTitle = ({ text }: CardTitleProps) => {
-	return <h2 className='text-3xl font-medium text-neutral-50 my-2'>{text}</h2>;
-};
-
-export default CardTitle;
